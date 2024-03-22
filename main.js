3b36b6061aca0419a493ebae64f0075d7ca6f272
Kaitl <117489101+Kaitl@users.noreply.github.com>
2024-03-22 18:55:59 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,55 @@
 var speakinghuman=window.webkitSpeechRecognition;
 var Bob=new speakinghuman
+var texttospeech=window.speechSynthesis
+
 
 function start() {
     Bob.start()
+}
+function selfie() {
+    Webcam.snap(function (data_uri) {
+        if (image_id=="selfie1") {
+            document.getElementById("result1").innerHTML="<img id='selfie1' src="+data_uri+">"   
+        }
+        if (image_id=="selfie2") {
+            document.getElementById("result2").innerHTML="<img id='selfie2' src="+data_uri+">"   
+        }
+        if (image_id=="selfie3") {
+            document.getElementById("result3").innerHTML="<img id='selfie3' src="+data_uri+">"   
+        }
+    })
+}
+function speak() {
+    speakingdata="taking a selfie in five seconds."
+    var sound_file=new SpeechSynthesisUtterance(speakingdata)
+    texttospeech.speak(sound_file)
+    Webcam.attach(camera);
+    setTimeout(function() {
+        image_id="selfie1"
+        selfie()
+        speakingdata="taking a selfie in ten seconds."
+    var sound_file=new SpeechSynthesisUtterance(speakingdata)
+    texttospeech.speak(sound_file)
+    }, 5000);
+    setTimeout(function() {
+        image_id="selfie2"
+        selfie()
+        speakingdata="taking a selfie in fifteen seconds."
+    var sound_file=new SpeechSynthesisUtterance(speakingdata)
+    texttospeech.speak(sound_file)
+    }, 10000);
+    setTimeout(function() {
+        image_id="selfie3"
+        selfie()
+    }, 15000);
+}
+Bob.onresult= function (event) {
+    console.log(event)
+    content=event.results[0][0].transcript
+    console.log(content)
+    if (content=="selfie") {
+        speak()
+    }
 }
 
 Webcam.set({
